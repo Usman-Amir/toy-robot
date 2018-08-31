b0a1afcd38f37a6e337a52e11399773285bfc474
UsmanAmir <muhammad.usman.amir@exact.com>
2018-08-31 19:24:40 +0800

    Initialize Table for the robot

---
+++ toy-robot/Table/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toy_robot
+{
+	class Table
+	{
+		public int BoxX, Boxy;
+		public Table(int x, int y)
+		{
+			//handle expectpion
+			this.BoxX = x;
+			this.Boxy = y;
+		}
+
+		 public bool IsPositionValid(Position position)
+		{
+			return !(position.x < 0 || position.y >= this.Boxy || position.y < 0 || position.y >= this.Boxy);
+		}
+	}
+}
