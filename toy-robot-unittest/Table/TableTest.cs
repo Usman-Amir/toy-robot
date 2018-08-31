32e20d1f617e43c2b25ff0ae3b51834045b283b0
UsmanAmir <muhammad.usman.amir@exact.com>
2018-08-31 21:12:40 +0800

    update test cases for table

---
+++ toy-robot-unittest/Table/TableTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using toy_robot;
+
+
+namespace toy_robot_unittest
+{
+	[TestClass]
+	public class TableTest
+	{
+		[TestMethod]
+		public void TablePosition_SetPositionofTable_RobotIsOnValidPosition()
+		{
+			//arrange
+			Table table = new Table(5, 5);
+			Position position = new Position(1,1, "NORTH");
+
+			//act
+			bool tablePosition = table.IsPositionValid(position);
+
+			//assert
+			Assert.AreEqual(true, tablePosition, "The position of robot is not on the table");
+		}
+	}
+}
