362f45e546fe59d4f33ce824e189438c3a02c809
UsmanAmir <muhammad.usman.amir@exact.com>
2018-08-31 19:23:49 +0800

    initialize position for the table

---
+++ toy-robot/Helper/Position.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toy_robot
+{
+	class Position
+	{
+		
+		public int x, y;
+		public string direction;
+		Position(int x, int y, string direction)
+		{
+			this.x = x;
+			this.y = y;
+			this.direction = direction;
+		}
+	}
+	class Direction
+	{
+		public enum directionEnum {
+			NORTH,
+			EAST,
+			SOUTH,
+			WEST
+		};
+	}
+
+}
