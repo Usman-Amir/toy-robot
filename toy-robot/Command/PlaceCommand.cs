3aa34657e89653af3e69eacd37012b2f5f9ec866
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 02:47:12 +0800

    Initialize PLACE command class

---
+++ toy-robot/Command/PlaceCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobot
+{
+	public class PlaceCommand : AbstractCommand
+	{
+		public PlaceCommand(Robot robot) : base(robot)
+		{
+		}
+
+		public override bool execute(Position position)
+		{
+			return base.execute(position);
+		}
+	}
+}
