68d55d6555b29f0a1d7519a56202e6fd3c1483b9
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-01 23:20:01 +0800

    initialize robot into the game

---
+++ toy-robot/Robot/Robot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyRobot;
+
+namespace toy_robot
+{
+	class Robot
+	{
+		public Position position;
+
+		public Robot()
+		{
+			position = new Position(0,0,"");
+		}
+		/*
+      Gets the current position of the robot.
+     */
+		public Position CurrentPosition
+		{
+			get
+			{
+				return position;
+			}
+		}
+		public bool SetPosition(Position position)
+		{
+			this.position = new Position(position.x, position.y, position.direction);
+			return true;
+		}
+
+		public bool IsAlive
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(this.position.direction))
+					return false;
+				else
+					return true;
+			}
+		}
+	}
+}
