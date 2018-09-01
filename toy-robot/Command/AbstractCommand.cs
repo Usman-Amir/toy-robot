4365af235bedc6de8fd52d3c4d0bcb6b339273af
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 02:12:52 +0800

    introduce a parent command class

--- toy-robot/Command/AbstractCommand.cs
+++ toy-robot/Command/AbstractCommand.cs
@@ -8,6 +8,15 @@ namespace ToyRobot
 {
 	public class AbstractCommand
 	{
-
+		public Robot robot;
+		public AbstractCommand(Robot robot)
+		{
+			this.robot = robot;
+		}
+		public bool execute(Position position)
+		{
+			Table table = new Table(position.x, position.y);
+			return table.IsPositionValid(position);
+		}
 	}
 }
