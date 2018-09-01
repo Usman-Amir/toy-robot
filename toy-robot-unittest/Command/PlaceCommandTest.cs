ab7622cb0f4353e16f1626bfc9ba40dded5b34d9
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 03:18:01 +0800

    Intialize test cases for PLACE command

---
+++ toy-robot-unittest/Command/PlaceCommandTest.cs
@@ -0,0 +1,36 @@
+﻿using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using Assert = NUnit.Framework.Assert;
+using ToyRobot;
+
+namespace ToyRobotTest
+{
+	[TestClass]
+	public class PlaceCommandTest
+	{
+		[TestMethod]
+		public void CommandForRobot_PlaceInvalidPositionOnTable_ShouldNotPlaceRobotForInvalidPositino()
+		{
+			Robot robot = new Robot();
+			PlaceCommand placecommand = new PlaceCommand(robot);
+			Table table = new Table(5, 5);
+			bool IsCommandPlaced;
+
+			IsCommandPlaced = placecommand.execute(new Position(11, 4, "NORTH"));
+
+			Assert.AreEqual(false,IsCommandPlaced, "The Place command is not valid");
+		}
+		[TestMethod]
+		public void CommandForRobot_PlaceValidPositionOnTable_ShouldPlaceRobotForValidPositino()
+		{
+			Robot robot = new Robot();
+			PlaceCommand placecommand = new PlaceCommand(robot);
+			Table table = new Table(5, 5);
+			bool IsCommandPlaced;
+
+			IsCommandPlaced = placecommand.execute(new Position(3, 4, "NORTH"));
+
+			Assert.AreEqual(false, IsCommandPlaced, "The Place command is not valid");
+		}
+	}
+}
