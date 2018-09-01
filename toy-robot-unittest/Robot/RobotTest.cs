3ffabc759eb2826da526ffa7a7b774cc400d7082
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 00:03:31 +0800

    enhance test cases for setting the position of robot

--- toy-robot-unittest/Robot/RobotTest.cs
+++ toy-robot-unittest/Robot/RobotTest.cs
@@ -23,5 +23,17 @@ namespace ToyRobotTest
 			Assert.AreEqual(0, position.y, "Current position of Robot at y axis is not on table");
 			Assert.AreEqual("", position.direction, "Current direction of table is not yet set");
 		}
+		public void RobotPosition_SettingThePositionOfRobot_RobotPositionShouldSet()
+		{
+			//Arrange
+			Robot robot = new Robot();
+			bool IsPositionSet;
+
+			//Act
+			IsPositionSet = robot.SetPosition(new Position(5, 5, "NORTH"));
+
+			//Assert
+			Assert.AreEqual(true, IsPositionSet, "New Position of robot not set");
+		}
 	}
 }
