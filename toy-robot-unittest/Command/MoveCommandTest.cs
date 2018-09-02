0ab970c6e0ec8c365451c0f48558cfedcd12a6db
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 16:50:05 +0800

    enriched test cases for some more scenarios

--- toy-robot-unittest/Command/MoveCommandTest.cs
+++ toy-robot-unittest/Command/MoveCommandTest.cs
@@ -41,5 +41,37 @@ namespace ToyRobotTest
 			//Assert
 			Assert.AreEqual(true, nextposition, "Please provide the valid new position for the robot to move");
 		}
+
+		[TestMethod]
+		public void MoveRobot_GettingTheNExtPossiblePositionForNextMove_ShouldMoveTheRobotOneStepWest()
+		{
+			//Arrange
+			Robot robot = new Robot();
+			MoveCommand moveCommand = new MoveCommand(robot);
+			Table table = new Table(5, 5);
+			Position nextposition;
+			
+			//Act
+			nextposition = moveCommand.getNextPosition(new Position(5, 5, "WEST"));
+
+			//Assert
+			Assert.AreEqual(4, nextposition.x, "The Next position in the west direction is not valid on the table");
+		}
+
+		[TestMethod]
+		public void MoveRobot_GettingTheNExtPossiblePositionForNextMove_ShouldMoveTheRobotOneStepEast()
+		{
+			//Arrange
+			Robot robot = new Robot();
+			MoveCommand moveCommand = new MoveCommand(robot);
+			Table table = new Table(5, 5);
+			Position nextposition;
+			
+			//Act
+			nextposition = moveCommand.getNextPosition(new Position(2, 3, "EAST"));
+
+			//Assert
+			Assert.AreEqual(3, nextposition.x, "The Next position in the east direction is not valid on table");
+		}
 	}
 }
