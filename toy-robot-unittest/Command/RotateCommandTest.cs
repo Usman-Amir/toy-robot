60c5f23f94746af36a8b606587b859e3338d0f3b
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 23:26:53 +0800

    initialize test cases for ROTATE command

---
+++ toy-robot-unittest/Command/RotateCommandTest.cs
@@ -0,0 +1,27 @@
+﻿using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using Assert = NUnit.Framework.Assert;
+using ToyRobot;
+
+namespace ToyRobotTest
+{
+	[TestClass]
+	public class RotateCommandTest
+	{
+		[TestMethod]
+		public void RotateRobot_GivenTheDirectionToNorth_RobotShouldMoveToWest()
+		{
+			Robot robot = new Robot();
+			new RotateCommand(robot).execute(new Position(3, 4, "NORTH"), -1);
+			Assert.AreEqual("WEST", robot.CurrentPosition.direction, "The Intended position of robot is not correct, it should be west");
+		}
+
+		[TestMethod]
+		public void RotateRobot_GivenTheDirectionToWest_RobotShouldMoveToNorth()
+		{
+			Robot robot = new Robot();
+			new RotateCommand(robot).execute(new Position(3, 4, "WEST"), 1);
+			Assert.AreEqual("NORTH", robot.CurrentPosition.direction, "The Intended position of robot is not correct, it should be north");
+		}
+	}
+}
