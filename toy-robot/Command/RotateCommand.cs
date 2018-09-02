af51e904ba5947aba489502e06a9bf0a3b0565ba
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 17:34:39 +0800

    initialize ROTATE Command

---
+++ toy-robot/Command/RotateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobot
+{
+	class RotateCommand : AbstractCommand
+	{
+		public RotateCommand(Robot robot) : base(robot)
+		{
+		}
+		public void execute(Position position, int step)
+		{
+			Position newPosition = position;
+			int index = (int)Enum.Parse(typeof(Direction.directionEnum), position.direction) + step;
+			if (index < 0)
+				index = Enum.GetNames(typeof(Direction.directionEnum)).Length - 1;
+			else
+				index = index % Enum.GetNames(typeof(Direction.directionEnum)).Length;
+
+			Direction.directionEnum de = (Direction.directionEnum)index;
+			newPosition.direction = de.ToString();
+			this.robot.SetPosition(newPosition);
+		}
+	}
+}
