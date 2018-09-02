8ae14ea260f7d7e15b2601f3fce8b43c3b2f22eb
UsmanAmir <muhammad.usman.amir@exact.com>
2018-09-02 15:58:45 +0800

    initialize test cases for MOVE command

---
+++ toy-robot-unittest/Command/MoveCommandTest.cs
@@ -0,0 +1,28 @@
+﻿using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using Assert = NUnit.Framework.Assert;
+using ToyRobot;
+
+namespace ToyRobotTest
+{
+	[TestClass]
+	public class MoveCommandTest
+	{
+		[TestMethod]
+		public void MoveRobot_MoveRobotToOneStepFromTheEdgeofTable_ShouldNotMoveTheRobotAsItWasAlreadyOnEdge()
+		{
+			//Arrange
+			Robot robot = new Robot();
+			MoveCommand moveCommand = new MoveCommand(robot);
+			Table table = new Table(5, 5);
+			Position nextposition;
+			bool IsValidPosition;
+			//Act
+			nextposition = moveCommand.getNextPosition(new Position(1, 1, "NORTH"));
+			IsValidPosition = moveCommand.execute(nextposition);
+
+			//Assert
+			Assert.AreEqual(false, nextposition, "Please provide the valid new position for the robot to move");
+		}
+	}
+}
